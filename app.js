b58b4faf69b422dcbf0646d7002d0b2ae8b023d4
ikoojoshi <ipshitamjohshi@gmail.com>
2020-05-16 21:40:52 +0300

    helpme

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const mongoose = require('mongoose');
 const readline = require('readline');
 const fs = require('fs');
 
-// const lineByLine = require('n-readlines');
-
 //connect to database
 mongoose.connect("mongodb://localhost/literacyforall", {useNewUrlParser:true, useUnifiedTopology: true});
 let db = mongoose.connection;
@@ -57,21 +55,90 @@ app.set('view engine', 'pug');
 
 //Home Routes
 app.get('/', function(req, res){
-  Course.find({}, function(err, courses){
-    if(err){
-      console.log(err);
-    }
-    else {
-      res.render('index',{
-        courses : courses
+
+  // function indexpage(course){
+  //   res.render('index',{
+  //     courses: course,
+  //   });
+  // }
+  const directoryPath = path.join(__dirname, 'public/data');
+  var course= [];
+
+  async function finaltry() {
+  fs.readdir(directoryPath, function getData(err, files) {
+      //handling error
+      if (err) {
+          return console.log('Unable to scan directory: ' + err);
+      }
+      //listing all files using forEach
+      var description;
+      var path2;
+      var title;
+      files.forEach(function (file) {
+          // Do whatever you want to do with the file
+
+          const fileStream = fs.createReadStream('public/data/'+file);
+          const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+          });
+          let i = 0
+          title = file.substring(0, file.length - 4);
+          console.log(title);
+
+          async function processLineByLine() {
+            for await (const line of rl) {
+              if(i==0){
+                description = line;
+              }
+              else if(i==1){
+                path2 = line;
+                course.push([{title: title, description: description, path: path2}]);
+              }
+              else
+                break;
+
+              i = i + 1;
+            }
+          }
+          processLineByLine();
       });
-    }
+      // res.render('index',{
+      //   courses: course,
+      // });
+
   });
-});
+  console.log(course);
+  return course;
+}
+//second async function
+async function second(){
+  let course = await finaltry();
+  console.log(course);
+  res.render('index',{
+    courses: course
+  });
+}
+
+
+
+  });
+
+  // Course.find({}, function(err, courses){
+  //   if(err){
+  //     console.log(err);
+  //   }
+  //   else {
+  //     res.render('index',{
+  //       courses : courses
+  //     });
+  //   }
+  // });
+//});
 
 // for various courses
-app.get('/course/:id', function(req,res){
-  const fileStream = fs.createReadStream('public/data/English.txt');
+app.get('/course/:title', function(req,res){
+  const fileStream = fs.createReadStream('public/data/'+req.params.title+'.txt');
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity
@@ -79,9 +146,7 @@ app.get('/course/:id', function(req,res){
   let i = 0
   var description;
   var path;
-  var content = [];
-  var videos = [];
-  var course = [];
+  var course= [];
   async function processLineByLine() {
     let i = 0
     for await (const line of rl) {
@@ -94,12 +159,10 @@ app.get('/course/:id', function(req,res){
       }
       else if(i%2==0){
         var c = line;
-        content.push(line);
       }
       else{
         var v = line;
         course.push({content: c, videos: v});
-        videos.push(line);
       }
         i = i + 1;
 
@@ -108,10 +171,8 @@ app.get('/course/:id', function(req,res){
     res.render('content',{
       description: description,
       path: path,
-      content: content,
-      videos: videos,
       course: course,
-      i: 0
+      title: req.params.title
     });
   }
   processLineByLine();
